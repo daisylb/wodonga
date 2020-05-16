904db08459a42df664f7f6c7b771de864b160003
Leigh Brenecki <l@leigh.net.au>
2020-05-16 20:10:38 +0930

    Switch to Trio

--- devprox/tcp.py
+++ devprox/tcp.py
@@ -1,34 +1,25 @@
-import asyncio
+import trio
 import re
-from subprocess import PIPE
 
 REDIR_RE = re.compile(r'([0-9a-f\:]+)\[([0-9]+)\] <- ([0-9a-f\:]+)\[([0-9]+)\] <- ([0-9a-f\:]+)\[([0-9]+)\]')
 
-async def tcp_connection_handler(reader, writer):
-    peer_ip, peer_port, *_ = writer.get_extra_info('peername')
-    self_ip, self_port, *_ = writer.get_extra_info('sockname')
-    writer.write(f'{peer_ip=}, {peer_port=}, {self_ip=}, {self_port=}\n'.encode('utf8'))
+async def tcp_connection_handler(stream):
+    peer_ip, peer_port, *_ = stream.socket.getpeername()
+    self_ip, self_port, *_ = stream.socket.getsockname()
+    await stream.send_all(f'{peer_ip=}, {peer_port=}, {self_ip=}, {self_port=}\n'.encode('utf8'))
 
-    process = await asyncio.create_subprocess_exec('sudo', 'pfctl', '-s', 'state', stdout=PIPE)
-    out, _ = await process.communicate()
-    print(out)
-    for match in REDIR_RE.finditer(out.decode('utf8')):
+    process = await trio.run_process(('sudo', 'pfctl', '-s', 'state'), capture_stdout=True)
+    for match in REDIR_RE.finditer(process.stdout.decode('utf8')):
         m_self_ip, m_self_port, m_target_ip, m_target_port, m_peer_ip, m_peer_port = match.groups()
         if m_self_ip == self_ip and int(m_self_port) == self_port and m_peer_ip == peer_ip and int(m_peer_port) == peer_port:
-            writer.write(f'{m_target_ip=}, {m_target_port=}\n'.encode('utf8'))
+            await stream.send_all(f'{m_target_ip=}, {m_target_port=}\n'.encode('utf8'))
             break
     else:
-        writer.write(f'not found'.encode('utf8'))
+        await stream.send_all(f'not found'.encode('utf8'))
     
 
 async def main():
-    server = await asyncio.start_server(tcp_connection_handler, host='::1', port=55555)
-
-    addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr}')
-
-    async with server:
-        await server.serve_forever()
+    await trio.serve_tcp(tcp_connection_handler, 55555, host='::1')
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    trio.run(main)
