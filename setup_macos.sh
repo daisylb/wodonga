520ec9d337570fa25c7f49b093359f8c4ebc2845
Leigh Brenecki <l@leigh.net.au>
2020-06-01 19:25:22 +0930

    macOS setup fixes

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 route add -inet6 fd7f:1fa7:68ca:202f:4b5c:aef6::/96 ::1
-echo -e "rdr pass proto tcp from any to fd7f:1fa7:68ca:202f:4b5c:aef6::/96 -> ::1 port 2000" | pfctl -a "com.apple/250.devprox" -Ef -
+echo "rdr pass inet6 proto tcp from any to fd7f:1fa7:68ca:202f:4b5c:aef6::/96 port 1:65535 -> ::1 port 55555" | pfctl -a "com.apple/250.devprox" -Ef -
+ifconfig feth99 create
+ifconfig feth99 inet6 fd77:1fa7:202f:acab::1 prefixlen 128
+scutil <<EOI
+d.init
+d.add Addresses * fd77:1fa7:202f:acab::1
+d.add DestAddresses * fd77:1fa7:202f:acab::1
+d.add Flags * 0 0
+d.add InterfaceName feth99
+d.add PrefixLength 128
+set State:/Network/Service/devprox-dummy-feth/IPv6
+set Setup:/Network/Service/devprox-dummy-feth/IPv6
+EOI
